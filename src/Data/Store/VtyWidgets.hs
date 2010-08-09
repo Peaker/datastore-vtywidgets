02d803b524e204cf17b7ff20ec3a116d9b3c467a
Eyal Lotem <eyal.lotem@gmail.com>
2010-08-10 01:32:10 +0300

    Add completion widget support

--- src/Data/Store/VtyWidgets.hs
+++ src/Data/Store/VtyWidgets.hs
@@ -3,18 +3,21 @@
 module Data.Store.VtyWidgets
     (MWidget, TWidget,
      appendBoxChild, popCurChild,
-     makeBox, makeTextEdit, makeChoiceWidget, widgetDownTransaction)
+     makeBox, makeTextEdit, makeCompletion, makeSimpleCompletion, makeChoiceWidget,
+     widgetDownTransaction)
 where
 
-import           Control.Monad                   (when, liftM)
-import qualified Graphics.Vty                    as Vty
-import qualified Graphics.UI.VtyWidgets.Box      as Box
-import qualified Graphics.UI.VtyWidgets.TextEdit as TextEdit
-import           Graphics.UI.VtyWidgets.Widget   (Widget)
-import qualified Data.Store.Property             as Property
-import qualified Data.Store.Transaction          as Transaction
-import           Data.Store.Transaction          (Transaction, Store)
-import           Data.Maybe                      (listToMaybe)
+import           Control.Monad                     (when, liftM)
+import           Data.Function.Utils               (result)
+import qualified Graphics.Vty                      as Vty
+import qualified Graphics.UI.VtyWidgets.Box        as Box
+import qualified Graphics.UI.VtyWidgets.TextEdit   as TextEdit
+import qualified Graphics.UI.VtyWidgets.Completion as Completion
+import           Graphics.UI.VtyWidgets.Widget     (Widget)
+import qualified Data.Store.Property               as Property
+import qualified Data.Store.Transaction            as Transaction
+import           Data.Store.Transaction            (Transaction, Store)
+import           Data.Maybe                        (listToMaybe)
 
 removeAt :: Int -> [a] -> [a]
 removeAt n xs = take n xs ++ drop (n+1) xs
@@ -59,13 +62,38 @@ makeBox orientation rows boxModelRef =
   Box.make orientation (Property.set boxModelRef) rows `liftM`
   Property.get boxModelRef
 
-makeTextEdit :: Monad m => Int -> Vty.Attr -> Vty.Attr ->
+makeWidget :: Monad m =>
+              (model -> Widget model) ->
+              Transaction.Property t m model ->
+              MWidget (Transaction t m)
+makeWidget w ref = (fmap (Property.set ref) . w) `liftM`
+                   Property.get ref
+
+makeTextEdit :: Monad m => String -> Int -> Vty.Attr -> Vty.Attr ->
                 Transaction.Property t m TextEdit.Model ->
                 MWidget (Transaction t m)
-makeTextEdit maxLines defAttr editAttr textEditModelRef =
-  liftM (fmap (Property.set textEditModelRef) .
-        TextEdit.make "<empty>" maxLines defAttr editAttr) $
-  Property.get textEditModelRef
+makeTextEdit =
+  (result . result . result . result) makeWidget TextEdit.make
+
+makeCompletion :: Monad m =>
+                  [(String, Int)] -> Int -> Vty.Color -> Vty.Attr -> Vty.Attr ->
+                  String -> Int -> Vty.Attr -> Vty.Attr ->
+                  Transaction.Property t m Completion.Model ->
+                  MWidget (Transaction t m)
+makeCompletion =
+  (result . result . result .
+   result . result . result .
+   result . result . result) makeWidget Completion.make
+
+makeSimpleCompletion :: Monad m =>
+                        [String] -> Int -> Vty.Color -> Vty.Attr -> Vty.Attr ->
+                        String -> Int -> Vty.Attr -> Vty.Attr ->
+                        Transaction.Property t m Completion.Model ->
+                        MWidget (Transaction t m)
+makeSimpleCompletion =
+  (result . result . result .
+   result . result . result .
+   result . result . result) makeWidget Completion.makeSimple
 
 makeChoiceWidget :: Monad m =>
                     Box.Orientation ->
@@ -80,6 +108,7 @@ makeChoiceWidget orientation keys boxModelRef = do
     maxIndex = length items - 1
     widgets = map fst keys
     items = map snd keys
+
 
 -- Take a widget parameterized on transaction on views (that lives in
 -- a nested transaction monad) and convert it to one parameterized on
